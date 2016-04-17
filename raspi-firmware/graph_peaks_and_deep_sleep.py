bea8e65abec0e20be7ddb3bcff005b3962ac8ebe
BensonQiu <qiu.benson@gmail.com>
2016-04-17 17:41:34 -0400

    Add REM analysis to algorithm

--- raspi-firmware/graph_peaks_and_deep_sleep.py
+++ raspi-firmware/graph_peaks_and_deep_sleep.py
@@ -4,9 +4,9 @@ import numpy as np
 from scipy.signal import find_peaks_cwt
 import random
 
-peak_threshold = 4
-deep_sleep_threshold = 7200
-
+peak_threshold = 2
+deep_sleep_threshold = 3600
+rem_sleep_threshold = 6400
 
 def set_to_list(vals):
     return sorted([val for val in vals])
@@ -16,6 +16,7 @@ def acc_algorithm(vals, timestamps):
     last_peak_time = timestamps[0];
     peaks = set()
     deep_sleep_times = set()
+    rem_sleep_times = set()
 
     for i in xrange(0, num_vals-10):
         next_ten_average = sum(vals[i:i+10])/10
@@ -23,7 +24,9 @@ def acc_algorithm(vals, timestamps):
             peaks.add(i)
             last_peak_time = timestamps[i]
         else:
-            if timestamps[i] - last_peak_time > deep_sleep_threshold:
+            if timestamps[i] - last_peak_time > rem_sleep_threshold:
+                rem_sleep_times.add(i)
+            elif timestamps[i] - last_peak_time > deep_sleep_threshold:
                 deep_sleep_times.add(i)
 
     for i in xrange(0, num_vals-10):
@@ -32,7 +35,9 @@ def acc_algorithm(vals, timestamps):
             peaks.add(i)
             last_peak_time = timestamps[i]
         else:
-            if timestamps[i] - last_peak_time > deep_sleep_threshold:
+            if timestamps[i] - last_peak_time > rem_sleep_threshold:
+                rem_sleep_times.add(i)
+            elif timestamps[i] - last_peak_time > deep_sleep_threshold:
                 deep_sleep_times.add(i)
 
     for i in xrange(10, num_vals):
@@ -42,6 +47,8 @@ def acc_algorithm(vals, timestamps):
             last_peak_time = timestamps[i]
         else:
             if timestamps[i] - last_peak_time > deep_sleep_threshold:
+                rem_sleep_times.add(i)
+            elif timestamps[i] - last_peak_time > deep_sleep_threshold:
                 deep_sleep_times.add(i)
 
     for i in xrange(10, num_vals):
@@ -51,9 +58,11 @@ def acc_algorithm(vals, timestamps):
             last_peak_time = timestamps[i]
         else:
             if timestamps[i] - last_peak_time > deep_sleep_threshold:
+                rem_sleep_times.add(i)
+            elif timestamps[i] - last_peak_time > deep_sleep_threshold:
                 deep_sleep_times.add(i)
 
-    return peaks, deep_sleep_times
+    return peaks, deep_sleep_times, rem_sleep_times
 
 filename = 'accOutputSmall.txt'
 
@@ -79,25 +88,36 @@ with open(filename) as f:
         timestamps.append(float(timestamp))
 
     int_timestamps = range(1,len(x_vals)+1)
-    x_peaks, x_deep_times = acc_algorithm(x_vals,int_timestamps)
-    y_peaks, y_deep_times = acc_algorithm(y_vals,int_timestamps)
-    z_peaks, z_deep_times = acc_algorithm(z_vals,int_timestamps)
+    x_peaks, x_deep_times, x_rem_times = acc_algorithm(x_vals,int_timestamps)
+    y_peaks, y_deep_times, y_rem_times = acc_algorithm(y_vals,int_timestamps)
+    z_peaks, z_deep_times, z_rem_times = acc_algorithm(z_vals,int_timestamps)
 
+
+    to_plot_x_rem_times = set_to_list(x_rem_times)
+    to_plot_x_rem_vals = [x_vals[i] for i in to_plot_x_rem_times]
 
     to_plot_x_deep_times = set_to_list(x_deep_times)
     to_plot_x_deep_vals = [x_vals[i] for i in to_plot_x_deep_times]
 
-    to_plot_x_light_times = set(int_timestamps).difference(x_deep_times)
+    to_plot_x_light_times = set(int_timestamps).difference(x_deep_times).difference(x_rem_times)
     to_plot_x_light_times.remove(len(x_vals))
     to_plot_x_light_times = set_to_list(to_plot_x_light_times)
 
     to_plot_x_light_vals = [x_vals[i] for i in to_plot_x_light_times]
 
+    light_to_deep_transitions = []
+    deep_to_light_transitions = []
+    for i in xrange(1,len(x_vals)):
+        if i in x_deep_times and not i-1 in x_deep_times:
+            light_to_deep_transitions.append(i)
+        elif i-1 in x_deep_times and not i in x_deep_times:
+            deep_to_light_transitions.append(i)
+
     plt.subplot(3,1,1)
     plt.title('x vs. Time')
-    plt.scatter(to_plot_x_deep_times, to_plot_x_deep_vals, color='red')
-    plt.scatter(to_plot_x_light_times, to_plot_x_light_vals, color='green')
-
+    plt.scatter(to_plot_x_rem_times, to_plot_x_rem_vals, color='blue', s=2)
+    plt.scatter(to_plot_x_deep_times, to_plot_x_deep_vals, color='red', s=2)
+    plt.scatter(to_plot_x_light_times, to_plot_x_light_vals, color='green', s=2)
 
 
 
@@ -112,8 +132,8 @@ with open(filename) as f:
 
     plt.subplot(3,1,2)
     plt.title('y vs. Time')
-    plt.scatter(to_plot_y_deep_times, to_plot_y_deep_vals, color='red')
-    plt.scatter(to_plot_y_light_times, to_plot_y_light_vals, color='green')
+    plt.scatter(to_plot_y_deep_times, to_plot_y_deep_vals, color='red', s=2)
+    plt.scatter(to_plot_y_light_times, to_plot_y_light_vals, color='green', s=2)
 
 
 
@@ -130,8 +150,8 @@ with open(filename) as f:
 
     plt.subplot(3,1,3)
     plt.title('z vs. Time')
-    plt.scatter(to_plot_z_deep_times, to_plot_z_deep_vals, color='red')
-    plt.scatter(to_plot_z_light_times, to_plot_z_light_vals, color='green')
+    plt.scatter(to_plot_z_deep_times, to_plot_z_deep_vals, color='red', s=2)
+    plt.scatter(to_plot_z_light_times, to_plot_z_light_vals, color='green', s=2)
 
 
     plt.show()
