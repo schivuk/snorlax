76e46071754d1322e9b932a6284b76fb80613597
Pratik Prakash <pratikprakash1@yahoo.com>
2016-04-21 17:19:42 -0400

    Fixed the alarm styles and added js functionality

--- webapp/snorlax/static/snorlax/js/alarm.js
+++ webapp/snorlax/static/snorlax/js/alarm.js
@@ -3,5 +3,37 @@ $(function() {
       on: 'Alarm On',
       off: 'Alarm Off',
       width: '400'
-    });
-})
+    });    
+});
+
+ function increment(field) {
+    if(field === 'hour') {
+        var div = $('#hour');
+        var hour = parseInt(div.val());
+        if(hour < 12) {
+            div.val(hour+1);
+        }
+    } else {
+        var div = $('#minute');
+        var min = parseInt(div.val());
+        if(min < 60) {
+            div.val(min+1);
+        }
+    }
+  }
+
+function decrement(field) {
+    if(field === 'hour') {
+        var div = $('#hour');
+        var hour = parseInt(div.val());
+        if(hour > 0) {
+            div.val(hour-1);
+        }
+    } else {
+        var div = $('#minute');
+        var min = parseInt(div.val());
+        if(min > 0) {
+            div.val(min-1);
+        }
+    }
+}
